fa6c7a87f64d62a5e6ac573a244d2dbdf682a71d
Rafael Barbosa <rafaelbarbosatec@gmail.com>
2019-04-16 11:59:21 -0300

    ajustes bloc

--- lib/pages/detail/DetailView.dart
+++ lib/pages/detail/DetailView.dart
@@ -11,14 +11,103 @@ class DetailView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: 100,
-        child: ImgHeroFromNetwork(
-            post.imgAuthor,
-           tagHero: "avatar${post.id}"
-        ),
-      ),
+      appBar: AppBar(),
+      body: _buildBody(),
     );
 
   }
+
+  _buildBody() {
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      child: Material(
+        color: Colors.white,
+        elevation: 3.0,
+        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+        child: ListView(
+          children: <Widget>[
+            _buildContent(),
+            _buildAuthor()
+          ],
+        ),
+      ),
+    );
+  }
+
+  _buildContent() {
+    if(post.attachments.length > 0){
+      return ClipRRect(
+        borderRadius: BorderRadius.only(topLeft: Radius.circular(10.0),topRight: Radius.circular(10.0)),
+        child: Container(
+          height: 200.0,
+          width: double.maxFinite,
+          child: ImgHeroFromNetwork(
+              post.attachments[0],
+              tagHero: "attachment${post.id}"
+          ),
+        ),
+      );
+    }else{
+      return Container();
+    }
+  }
+
+  _buildAuthor() {
+    return Align(
+      alignment: Alignment.topLeft,
+      child: Container(
+        padding: EdgeInsets.all(15.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            _buildImgAuthor(),
+            _buildNameAuthor()
+          ],
+        ),
+      ),
+    );
+  }
+
+  _buildImgAuthor() {
+    return Container(
+      width: 50.0,
+      height: 50.0,
+      child: ClipRRect(
+        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+        child: ImgHeroFromNetwork(
+            post.imgAuthor,
+            tagHero: "avatar${post.id}"
+        ),
+      ),
+    );
+  }
+
+  _buildNameAuthor() {
+    return Expanded(
+      child: Padding(
+        padding: EdgeInsets.only(left: 10.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              post.author,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey[800]
+              ),
+            ),
+            Text(
+              dateTransform(post.date),
+              style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 14.0
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
 }
